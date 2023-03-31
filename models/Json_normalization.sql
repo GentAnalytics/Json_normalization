f3e08015e8240d773d5abe30255cb7ad85664c12
analytics.ghent@iodigital.com <analytics.ghent@iodigital.com>
2023-03-31 13:05:20 +0000

    testing it

---
+++ models/Json_normalization.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }}
+
+with data_json as (
+    SELECT _airbyte_data
+    id, SAFE.PARSE_JSON(_airbyte_data) AS data_json_format
+  FROM
+   Customer_churn._airbyte_raw_customer_id_day
+
+)
+
+
+
+select
+JSON_VALUE(data_json_format.date) date,
+JSON_VALUE(data_json_format.sessions) sessions,
+JSON_VALUE(data_json_format.uuid) uuid,
+JSON_VALUE(data_json_format, '$.customUser:user_id') AS User_id
+
+
+from data_json
+
+
