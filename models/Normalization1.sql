5d281960031730b9a747207745f3990531d85418
analytics.ghent@iodigital.com <analytics.ghent@iodigital.com>
2023-04-21 13:02:48 +0000

    normalization 1

--- models/Normalization1.sql
+++ models/Normalization1.sql
@@ -13,6 +13,7 @@ with data_json as (
 select
 
 PARSE_DATE('%Y%m%d', JSON_VALUE(data_json_format.date)) as date,
+JSON_VALUE(data_json_format,'$.customUser:user_id') AS id,
 JSON_VALUE(data_json_format.sessions) sessions,
 JSON_VALUE(data_json_format.hour) hour,
 JSON_VALUE(data_json_format.pageTitle) pageTitle,
@@ -20,8 +21,6 @@ JSON_VALUE(data_json_format.eventName) eventName,
 JSON_VALUE(data_json_format, '$.customEvent:program_name') AS program_name,
 JSON_VALUE(data_json_format, '$.customEvent:program_variant') AS program_variant,
 JSON_VALUE(data_json_format, '$.customEvent:video_duration') AS video_duration,
-JSON_VALUE(data_json_format, '$.customUser:user_id') AS userid,
-JSON_VALUE(data_json_format.uuid) uuid,
 JSON_VALUE(data_json_format.property_id) property_id,
 from data_json
 
