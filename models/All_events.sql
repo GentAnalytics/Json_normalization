3af94b959a0ea6d235296463ccdd8ebb9566e4e6
analytics.ghent@iodigital.com <analytics.ghent@iodigital.com>
2023-04-25 11:44:43 +0000

    changing to see if user id comes out

--- models/All_events.sql
+++ models/All_events.sql
@@ -8,18 +8,17 @@ with data_json as (
 
 )
 
-
+[{"name": "Allevents", "dimensions": [ "date","hour","eventName","customUser:user_id", "pageTitle","customEvent:program_name","customEvent:program_variant"], "metrics": ["sessions"] }]
 
 select
-
 PARSE_DATE('%Y%m%d', JSON_VALUE(data_json_format.date)) as date,
-JSON_VALUE(data_json_format,'$.customUser:user_id') AS id,
-JSON_VALUE(data_json_format.sessions) sessions,
 JSON_VALUE(data_json_format.hour) hour,
 JSON_VALUE(data_json_format.eventName) eventName,
+JSON_VALUE(data_json_format,'$.customUser:user_id') AS userid,
+JSON_VALUE(data_json_format.pageTitle) pageTitle,
 JSON_VALUE(data_json_format, '$.customEvent:program_name') AS program_name,
 JSON_VALUE(data_json_format, '$.customEvent:program_variant') AS program_variant,
-JSON_VALUE(data_json_format, '$.customEvent:video_duration') AS video_duration,
+JSON_VALUE(data_json_format.sessions) sessions,
 JSON_VALUE(data_json_format.property_id) property_id,
 JSON_VALUE(data_json_format.uuid) uuid
 from data_json
