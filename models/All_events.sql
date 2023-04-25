6b3f825bfbc2496c8c6e65095f8f8af0e4d447d7
analytics.ghent@iodigital.com <analytics.ghent@iodigital.com>
2023-04-25 11:52:59 +0000

    changing session to active users

--- models/All_events.sql
+++ models/All_events.sql
@@ -18,7 +18,7 @@ JSON_VALUE(data_json_format,'$.customUser:user_id') AS userid,
 JSON_VALUE(data_json_format.pageTitle) pageTitle,
 JSON_VALUE(data_json_format, '$.customEvent:program_name') AS program_name,
 JSON_VALUE(data_json_format, '$.customEvent:program_variant') AS program_variant,
-JSON_VALUE(data_json_format.sessions) sessions,
+JSON_VALUE(data_json_format.activeUsers) activeusers,
 JSON_VALUE(data_json_format.property_id) property_id,
 JSON_VALUE(data_json_format.uuid) uuid
 from data_json
