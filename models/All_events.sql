6f12315162201dfc025bc46f54e25451c83966cf
analytics.ghent@iodigital.com <analytics.ghent@iodigital.com>
2023-05-08 15:30:48 +0000

    unsaved became save

--- models/All_events.sql
+++ models/All_events.sql
@@ -1,25 +1,25 @@
-{{ config(materialized='table') }}
+-- {{ config(materialized='table') }}
 
-with data_json as (
-    SELECT _airbyte_data
-    id, SAFE.PARSE_JSON(_airbyte_data) AS data_json_format
-  FROM
-   Export1._airbyte_raw_allevents
+-- with data_json as (
+--     SELECT _airbyte_data
+--     id, SAFE.PARSE_JSON(_airbyte_data) AS data_json_format
+--   FROM
+--    Export1._airbyte_raw_allevents
 
-)
+-- )
 
 
 
-select
-PARSE_DATE('%Y%m%d', JSON_VALUE(data_json_format.date)) as date,
-JSON_VALUE(data_json_format.hour) hour,
-JSON_VALUE(data_json_format.eventName) eventName,
-JSON_VALUE(data_json_format,'$.customUser:user_id') AS userid,
--- JSON_VALUE(data_json_format.pageTitle) pageTitle,
--- JSON_VALUE(data_json_format, '$.customEvent:program_name') AS program_name,
--- JSON_VALUE(data_json_format, '$.customEvent:program_variant') AS program_variant,
-JSON_VALUE(data_json_format.sessions) sessions,
-JSON_VALUE(data_json_format.property_id) property_id,
-JSON_VALUE(data_json_format.uuid) uuid
-from data_json
+-- select
+-- PARSE_DATE('%Y%m%d', JSON_VALUE(data_json_format.date)) as date,
+-- JSON_VALUE(data_json_format.hour) hour,
+-- JSON_VALUE(data_json_format.eventName) eventName,
+-- JSON_VALUE(data_json_format,'$.customUser:user_id') AS userid,
+-- -- JSON_VALUE(data_json_format.pageTitle) pageTitle,
+-- -- JSON_VALUE(data_json_format, '$.customEvent:program_name') AS program_name,
+-- -- JSON_VALUE(data_json_format, '$.customEvent:program_variant') AS program_variant,
+-- JSON_VALUE(data_json_format.sessions) sessions,
+-- JSON_VALUE(data_json_format.property_id) property_id,
+-- JSON_VALUE(data_json_format.uuid) uuid
+-- from data_json
 
