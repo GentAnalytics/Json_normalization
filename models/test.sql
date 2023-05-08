8eb3ca0e3a23fa4f9b91437a4b10e1839485c0bd
analytics.ghent@iodigital.com <analytics.ghent@iodigital.com>
2023-05-08 16:29:35 +0000

    final commitement for basic metric

--- models/test.sql
+++
@@ -1,8 +0,0 @@
-with first_json as (
-    SELECT _airbyte_data
-    id, SAFE.PARSE_JSON(_airbyte_data) AS data_json_format
-  FROM
-   Export1._airbyte_raw_basicmetricsmonthly
-
-),
-select * from first_json
